zmedico/filebus

commit a708fdec0f8f32334ce68c38bada59b2875397ad

Add bash implementation of filebus, accessible via --impl=bash

lib/bash/filebus.bash
@@ -1,0 +1,407 @@
+#!/usr/bin/env bash
+
+filebus_lib-init() {
+	local import_path
+
+	FILEBUS_IMPORT_SEARCH_PATHS=(
+		"${BASH_SOURCE[0]%/*}"
+	)
+
+	if ! declare -f filebus-bash-import_lib-init >/dev/null; then
+		for import_path in "${FILEBUS_IMPORT_SEARCH_PATHS[@]}"; do
+			[[ -e $import_path/bash-import.bash ]] || continue
+			# shellcheck disable=SC1090
+			source "$import_path/bash-import.bash" || return
+			filebus-bash-import_lib-init || return
+			break
+		done
+	fi
+
+	filebus-bash-import_search-paths-set "${FILEBUS_IMPORT_SEARCH_PATHS[@]}" || return
+	filebus-bash-import_import log signal supervisor
+
+	FILEBUS_CMD_ACTIONS="consumer|producer"
+	declare -g -A FILEBUS_DEFAULTS
+	FILEBUS_DEFAULTS[back_pressure]=0
+	FILEBUS_DEFAULTS[block_size]=4096
+	FILEBUS_DEFAULTS[blocking_read]=0
+	FILEBUS_DEFAULTS[file_monitoring]=1
+	FILEBUS_DEFAULTS[sleep_interval]=0.1
+	FILEBUS_DEFAULTS[verbosity]=0
+}
+
+# @FUNCTION: _filebus_set-action
+# @DESCRIPTION:
+# Set program action.
+_filebus_set-action() {
+	[[ -z ${filebus_args[action]:-} ]] || die "Multiple actions specified:" "${filebus_args[action]:-}" "${1}"
+	filebus_args[action]="${1}"
+}
+
+# @FUNCTION: _filebus_print-usage
+# @DESCRIPTION:
+# Print program usage instructions.
+_filebus_print-usage() {
+	printf -- 'usage: filebus [-h] [--back-pressure] [--block-size N] [--lossless] [--no-file-monitoring] [--filename FILE] [--sleep-interval N] [-v] {producer,consumer} ...\n'
+	printf -- '\n'
+	printf -- '  filebus VERSION (bash implementation)\n'
+	printf -- '  A user space multicast named pipe implementation backed by a regular file\n'
+	printf -- '\n'
+	printf -- 'positional arguments:\n'
+	printf -- '  {producer,consumer}\n'
+	printf -- '    producer            connect producer side of stream\n'
+	printf -- '    consumer            connect consumer side of stream\n'
+	printf -- '\n'
+	printf -- 'optional arguments:\n'
+	printf -- '  -h, --help            show this help message and exit\n'
+	printf -- '  --back-pressure       enable lossless back pressure protocol (unconsumed chunks cause producers to block)\n'
+	printf -- '  --block-size N        maximum block size in units of bytes\n'
+	printf -- '  --lossless            an alias for --back-pressure\n'
+	printf -- '  --no-file-monitoring  disable filesystem event monitoring\n'
+	printf -- '  --filename FILE       path of the data file (the producer updates it via atomic rename)\n'
+	printf -- '  --sleep-interval N    check for new messages at least once every N seconds\n'
+	printf -- '  -v, --verbose         verbose logging (each occurence increases verbosity)\n'
+}
+
+# @FUNCTION: _filebus_print-usage-producer
+# @DESCRIPTION:
+# Producer usage instructions.
+_filebus_print-usage-producer() {
+	printf -- 'usage: filebus producer [-h] [--blocking-read]\n'
+	printf -- '\n'
+	printf -- 'optional arguments:\n'
+	printf -- '  -h, --help       show this help message and exit\n'
+	printf -- '  --blocking-read  blocking read from input (clear the O_NONBLOCK flag)\n'
+}
+
+# @FUNCTION: _filebus_command-perform-producer
+# @DESCRIPTION:
+# Producer command.
+_filebus_command-perform-producer() {
+	local result
+	mkdir -p "${filebus_args[filename]%/*}" || return
+
+	while true; do
+		if (( filebus_args[back_pressure] == 1 )); then
+			while [[ -e ${filebus_args[filename]} ]]; do
+				sleep "${filebus_args[sleep_interval]}" || return
+			done
+		fi
+		buffer=()
+		buffer_len=0
+		! [[ -f /dev/fd/0 ]]
+		regular_file=$?
+		while true; do
+			if (( regular_file == 1 )) || read -r -t 0; then
+				read_ready=1
+				read -r
+				eof=$?
+				if (( eof != 1 )); then
+					buffer+=("${REPLY}")
+					(( buffer_len += ${#REPLY} ))
+					(( buffer_len++ )) # count newline
+				fi
+			else
+				read_ready=0
+				eof=0
+			fi
+
+			# Exit before the lock when possible (not possible when back pressure protocol is enabled).
+			if (( filebus_args[back_pressure] == 0 && eof == 1 && read_ready == 1 && buffer_len == 0 )); then
+				return 0
+			fi
+
+			if (( eof == 1 )) || (( read_ready == 0 && buffer_len >= 0 )) || (( buffer_len >= filebus_args[block_size] )); then
+				({
+					(
+						for line in "${buffer[@]}"; do
+							printf -- '%s\n' "$line" || exit 1
+						done
+					) > "${filebus_args[filename]}.__new__" || exit 1
+					mv "${filebus_args[filename]}"{.__new__,} || exit 1
+					if (( filebus_args[back_pressure] == 0 )); then
+						exit 0
+					elif [[ ! -s ${filebus_args[filename]} ]]; then
+						exit 2 # back pressure protocol EOF marker
+					fi
+					exit 0
+				} 200>"${filebus_args[filename]}.lock")
+				case $? in
+					0)
+						buffer=()
+						buffer_len=0
+						;;
+					1)
+						return 1
+						;;
+					2)	# back pressure protocol EOF marker
+						return 0
+						;;
+				esac
+			fi
+			if (( buffer_len == 0 )); then
+				sleep "${filebus_args[sleep_interval]}" || return
+			fi
+		done
+	done
+}
+
+# @FUNCTION: _filebus_command-parser-producer
+# @DESCRIPTION:
+# Parser for producer command arguments.
+_filebus_command-parser-producer() {
+	local consumed_args=0
+	while (( $# > 0 )); do
+		if [[ $1 =~ ^(-h|--help) ]]; then
+			_filebus_print-usage-producer || return 1
+			return 0
+		elif [[ $1 == "--blocking-read" ]]; then
+			filebus_args[blocking_read]=1
+			(( consumed_args += 1 ))
+			shift
+		else
+			# break for first unconsumed argument
+			break
+		fi
+	done
+
+	# status
+	# 0       immediate successful exit
+	# 1       immediate failure
+	# 2+      status = consumed_args + 2
+	return $(( consumed_args + 2 ))
+}
+
+# @FUNCTION: _filebus_print-usage-consumer
+# @DESCRIPTION:
+# Consumer usage instructions.
+_filebus_print-usage-consumer() {
+	printf -- 'usage: filebus consumer [-h]\n'
+	printf -- '\n'
+	printf -- 'optional arguments:\n'
+	printf -- '  -h, --help       show this help message and exit\n'
+}
+
+# @FUNCTION: _filebus_command-perform-consumer
+# @DESCRIPTION:
+# Consumer command.
+_filebus_command-perform-consumer() {
+	local data status
+	mkdir -p "${filebus_args[filename]%/*}" 2>/dev/null
+	while true; do
+		if [[ ! -e ${filebus_args[filename]} ]]; then
+			sleep "${filebus_args[sleep_interval]}" || return
+			continue
+		fi
+		if (( filebus_args[back_pressure] == 1 )); then
+			(
+				{
+					flock --exclusive 200 || exit 1
+					[[ "${filebus_args[filename]}.lock" -ef /dev/fd/200 ]] || exit 1
+					if [[ ! -e "${filebus_args[filename]}" ]]; then
+						exit 0
+					fi
+					[[ -s ${filebus_args[filename]} ]]
+					eof=$?
+					cat "${filebus_args[filename]}" || exit 1
+					rm -f "${filebus_args[filename]}" || exit 1
+					case $eof in
+						0)
+							exit 2
+							;;
+						1)
+							exit 3
+							;;
+						*)
+							exit 1
+					esac
+				} 200>"${filebus_args[filename]}.lock"
+			)
+			case $? in
+				1)
+					return 1
+					;;
+				0|2)
+					continue
+					;;
+				3)
+					return 0
+			esac
+		else
+			(
+				if ! exec < "${filebus_args[filename]}"; then
+					if [[ ! -d ${filebus_args[filename]%/*} ]]; then
+						exit 1
+					fi
+					sleep "${filebus_args[sleep_interval]}" || exit 1
+					exit 0
+				fi
+				while read -r; do
+					printf -- '%s\n' "$REPLY" || exit 1
+				done
+				while [[ ${filebus_args[filename]} -ef /dev/fd/0 ]]; do
+					sleep "${filebus_args[sleep_interval]}" || exit 1
+				done
+				exit 0
+			) || return
+		fi
+	done
+}
+
+# @FUNCTION: _filebus_command-parser-consumer
+# @DESCRIPTION:
+# Parser for query command arguments.
+_filebus_command-parser-consumer() {
+	local consumed_args=0
+	while (( $# > 0 )); do
+		if [[ $1 =~ ^(-h|--help) ]]; then
+			_filebus_print-usage-consumer || return 1
+			return 0
+		else
+			# break for first unconsumed argument
+			break
+		fi
+	done
+
+	# status
+	# 0       immediate successful exit
+	# 1       immediate failure
+	# 2+      status = consumed_args + 2
+	return $(( consumed_args + 2 ))
+}
+
+# @FUNCTION: filebus_main
+# @DESCRIPTION:
+# Main program.
+filebus_main() {
+	local arg sub_parser sub_parser_status
+	local -A filebus_args
+	filebus_args[back_pressure]=${FILEBUS_DEFAULTS[back_pressure]}
+	filebus_args[block_size]=${FILEBUS_DEFAULTS[block_size]}
+	filebus_args[blocking_read]=${FILEBUS_DEFAULTS[blocking_read]}
+	filebus_args[file_monitoring]=${FILEBUS_DEFAULTS[file_monitoring]}
+	filebus_args[sleep_interval]=${FILEBUS_DEFAULTS[sleep_interval]}
+	filebus_args[verbosity]=${FILEBUS_DEFAULTS[verbosity]}
+	shopt -s lastpipe
+
+	params=$(getopt -o "fhv" -l "back-pressure,block-size:,blocking-read,filename:,help,impl:,implementation:,lossless,no-file-monitoring,sleep-interval:,verbose" --name "${0##*/}" -- "${@}") || exit $?
+	eval set -- "${params}"
+
+	for arg in "${@}"; do
+		if [[ $arg =~ ^($FILEBUS_CMD_ACTIONS)$ ]]; then
+			if declare -f "_filebus_command-parser-$arg" >/dev/null; then
+				sub_parser=_filebus_command-parser-$arg
+			fi
+			break
+		fi
+	done
+
+	while [[ ${#} -gt 0 ]]; do
+
+		if [[ -n ${sub_parser:-} ]]; then
+			"$sub_parser" "$@"
+			sub_parser_status=$?
+
+			# account for any consumed arguments
+			while (( sub_parser_status > 2 )); do
+				(( sub_parser_status-- ))
+				shift
+			done
+
+			case $sub_parser_status in
+				0)
+					# immediate success
+					return 0
+					;;
+				1)
+					# immediate failure
+					return 1
+					;;
+				2)
+					# status = consumed_args + 2
+					;;
+			esac
+		fi
+
+		(( $# > 0 )) || break
+
+		case $1 in
+			-v|--verbose)
+				FILEBUS_DEFAULTS[verbosity]=$(( FILEBUS_DEFAULTS[verbosity] + 1 ))
+				if (( FILEBUS_DEFAULTS[verbosity] > 1 )); then
+					filebus-log_set-level debug
+				fi
+				;;
+			-h|--help)
+				_filebus_print-usage
+				exit 0
+				;;
+			--impl|--implementation)
+				case $2 in
+					bash)
+						shift
+						;;
+					*)
+						die "Not implemented: $1=$2"
+				esac
+				;;
+			--back-pressure|--lossless)
+				filebus_args[back_pressure]=1
+				;;
+			--block-size)
+				filebus_args[block_size]=$2
+				shift
+				;;
+			--filename)
+				filebus_args[filename]=$2
+				shift
+				;;
+			--no-file-monitoring)
+				unset "filebus_args[file_monitoring]"
+				;;
+			--sleep-interval)
+				filebus_args[sleep_interval]=$2
+				shift
+				;;
+			--)
+				;;
+			producer|consumer)
+				_filebus_set-action "${1}"
+				;;
+			*)
+				die "Unexpected argument: $1"
+				;;
+		esac
+		shift
+	done
+
+	if [[ -z ${filebus_args[action]:-} ]]; then
+		_filebus_print-usage
+		die "Missing required action: $FILEBUS_CMD_ACTIONS"
+	fi
+
+	if [[ -z ${filebus_args[filename]:-} ]]; then
+		_filebus_print-usage
+		die "Missing required --filename argument"
+	fi
+
+	cd /
+	filebus-signal_propagate SIGTERM SIGINT
+
+	filebus-log_line "debug" "action: ${filebus_args[action]:-}"
+
+	"_filebus_command-perform-${filebus_args[action]}" || return
+}
+
+if [[ $0 == "${BASH_SOURCE[0]}" ]]; then
+	set -u -o pipefail
+
+	if [[ ${BASH_SOURCE[0]} != /* ]]; then
+		canonical_source=$(realpath --canonicalize-missing "${BASH_SOURCE[0]}")
+		cd /
+		exec "$BASH" "$canonical_source" "${@}" || exit 1
+	fi
+
+	filebus_lib-init
+	filebus_main "$@"
+fi

lib/python/filebus/__init__.py
@@ -2,11 +2,14 @@ import argparse
 import array
 import asyncio
 import functools
+import glob
 import logging
 import os
+import shutil
 import signal
 import stat
 import sys
+import sysconfig
 
 try:
     import fcntl
@@ -22,7 +25,7 @@ except ImportError:
     watchdog = None
     FileSystemEventHandler = object
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __project__ = "filebus"
 __description__ = (
     "A user space multicast named pipe implementation backed by a regular file"
@@ -371,6 +374,17 @@ def parse_args(argv=None):
         prog=os.path.basename(argv[0]),
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="  {} {}\n  {}".format(__project__, __version__, __description__),
+        add_help=False,
+    )
+    root_parser.set_defaults(func=lambda args: None)
+
+    root_parser.add_argument(
+        "-h",
+        "--help",
+        dest="help",
+        action="store_true",
+        default=argparse.SUPPRESS,
+        help="show this help message and exit",
     )
 
     root_parser.add_argument(
@@ -388,6 +402,16 @@ def parse_args(argv=None):
         type=int,
         default=BUFSIZE,
         help="maximum block size in units of bytes",
+    )
+
+    root_parser.add_argument(
+        "--impl",
+        "--implementation",
+        dest="impl",
+        action="store",
+        choices=("bash", "python"),
+        default="python",
+        help="choose an alternative filebus implementation (alternative implementations interoperate with eachother)",
     )
 
     root_parser.add_argument(
@@ -451,6 +475,16 @@ def parse_args(argv=None):
     args = root_parser.parse_args(argv[1:])
     args.func(args)
 
+    if getattr(args, "help", False) and not args.impl == "bash":
+        if getattr(args, "command", None) == "consumer":
+            current_parser = consumer_parser
+        elif getattr(args, "command", None) == "producer":
+            current_parser = producer_parser
+        else:
+            current_parser = root_parser
+        current_parser.print_help()
+        current_parser.exit()
+
     logging.basicConfig(
         level=(logging.getLogger().getEffectiveLevel() - 10 * args.verbosity),
         format="[%(levelname)-4s] %(message)s",
@@ -459,8 +493,46 @@ def parse_args(argv=None):
     return args
 
 
-def main():
-    args = parse_args()
+def filebus_bash_impl(args):
+    bash_prog = shutil.which("bash")
+    if bash_prog is None:
+        raise FileNotFoundError("bash")
+    search_paths = [
+        os.path.join(sys.prefix, "libexec", "filebus", "filebus.bash"),
+    ]
+    if not __file__.startswith(sysconfig.get_path("purelib") + "/"):
+        source_path = os.path.join(
+            os.path.realpath(__file__).rpartition("/lib/")[0], "lib/bash/filebus.bash"
+        )
+        if not os.path.isfile(source_path):
+            source_path = next(
+                glob.iglob(
+                    os.path.join(
+                        os.path.realpath(__file__).rpartition("/lib/")[0],
+                        "../filebus-*/lib/bash/filebus.bash",
+                    )
+                ),
+                None,
+            )
+        if source_path is not None:
+            search_paths.insert(0, source_path)
+
+    for filebus_bash in search_paths:
+        if os.path.isfile(filebus_bash):
+            return [bash_prog, filebus_bash] + args
+
+    raise FileNotFoundError("filebus.bash" + " " + repr(search_paths))
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    args = parse_args(argv=argv)
+    if args.impl == "bash":
+        new_argv = filebus_bash_impl(argv[1:])
+        os.execvp(new_argv[0], new_argv)
+
     loop = asyncio.get_event_loop()
 
     try:

lib/bash/bash-import.bash
@@ -1,0 +1,100 @@
+#!/usr/bin/env bash
+
+# @FUNCTION: import_lib-init
+# @DESCRIPTION:
+# Library init function.
+filebus-bash-import_lib-init() {
+	filebus-bash-import_search-paths-reset
+	filebus-bash-import_lib-init-patterns-reset
+}
+
+# @FUNCTION: filebus-bash-import_import
+# @DESCRIPTION:
+# Import bash libs and call their lib-init functions if those functions
+# are not yet defined in the current environment.
+filebus-bash-import_import() {
+	local dir lib match pattern
+	for lib in "$@"; do
+		match=0
+		for pattern in "${FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS[@]}"; do
+			if eval "declare -f \"$pattern\"" >/dev/null; then
+				match=1
+				break
+			fi
+		done
+		if (( match != 1 )); then
+			for dir in "${FILEBUS_BASH_IMPORT_SEARCH_PATHS[@]}"; do
+				[[ -f $dir/${lib}.bash ]] || continue
+				# shellcheck disable=SC1090
+				source "$dir/$lib.bash" || return
+				for pattern in "${FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS[@]}"; do
+					if eval "declare -f \"$pattern\"" >/dev/null; then
+						match=1
+						eval "$pattern" || return
+						break
+					fi
+				done
+				if (( match != 1 )); then
+					echo "Failed to init $lib lib" >&2; return 1
+				fi
+				break
+			done
+		fi
+	done
+}
+
+# @FUNCTION: filebus-bash-import_search-paths-extend
+# @DESCRIPTION:
+# Extend FILEBUS_BASH_IMPORT_SEARCH_PATHS.
+filebus-bash-import_search-paths-extend() {
+	FILEBUS_BASH_IMPORT_SEARCH_PATHS+=("$@")
+}
+
+# @FUNCTION: filebus-bash-import_search-paths-list
+# @DESCRIPTION:
+# List current FILEBUS_BASH_IMPORT_SEARCH_PATHS, delimited by newlines.
+filebus-bash-import_search-paths-list() {
+	printf -- '%s\n' "${FILEBUS_BASH_IMPORT_SEARCH_PATHS[@]}"
+}
+
+# @FUNCTION: filebus-bash-import_search-paths-reset
+# @DESCRIPTION:
+# Reset FILEBUS_BASH_IMPORT_SEARCH_PATHS to defaults.
+filebus-bash-import_search-paths-reset() {
+	declare -g -a FILEBUS_BASH_IMPORT_SEARCH_PATHS=("${BASH_SOURCE[0]%/*}")
+}
+
+# @FUNCTION: filebus-bash-import_search-paths-set
+# @DESCRIPTION:
+# Set FILEBUS_BASH_IMPORT_SEARCH_PATHS from arguments.
+filebus-bash-import_search-paths-set() {
+	declare -g -a FILEBUS_BASH_IMPORT_SEARCH_PATHS=("$@")
+}
+
+# @FUNCTION: filebus-bash-import_lib-init-patterns-extend
+# @DESCRIPTION:
+# Extend FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS.
+filebus-bash-import_lib-init-patterns-extend() {
+	FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS+=("$@")
+}
+
+# @FUNCTION: filebus-bash-import_lib-init-patterns-list
+# @DESCRIPTION:
+#List FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS, delimited by newlines.
+filebus-bash-import_lib-init-patterns-list() {
+	printf -- '%s\n' "${FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS[@]}"
+}
+
+# @FUNCTION: filebus-bash-import_lib-init-patterns-reset
+# @DESCRIPTION:
+# Reset FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS to defaults.
+filebus-bash-import_lib-init-patterns-reset() {
+	declare -g -a FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS=('filebus-${lib#filebus-}_lib-init')
+}
+
+# @FUNCTION: filebus-bash-import_lib-init-patterns-set
+# @DESCRIPTION:
+# Set FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS from arguments.
+filebus-bash-import_lib-init-patterns-set() {
+	declare -g -a FILEBUS_BASH_IMPORT_LIB_INIT_PATTERNS=("$@")
+}

lib/bash/filebus-test.bash
@@ -1,0 +1,398 @@
+#!/usr/bin/env bash
+
+filebus-test_lib-init() {
+	local import_path
+
+	FILEBUS_TEST_IMPORT_SEARCH_PATHS=(
+		"${BASH_SOURCE[0]%/*}"
+	)
+
+	if ! declare -f filebus-bash-import_lib-init >/dev/null; then
+		for import_path in "${FILEBUS_TEST_IMPORT_SEARCH_PATHS[@]}"; do
+			[[ -e $import_path/bash-import.bash ]] || continue
+			# shellcheck disable=SC1090
+			source "$import_path/bash-import.bash" || return
+			filebus-bash-import_lib-init || return
+			break
+		done
+	fi
+
+	filebus-bash-import_search-paths-set "${FILEBUS_TEST_IMPORT_SEARCH_PATHS[@]}" || return
+	filebus-bash-import_import log signal supervisor
+
+	FILEBUS_TEST_CMD_ACTIONS="test"
+}
+
+# @FUNCTION: filebus
+# @DESCRIPTION:
+# Call filebus (bash implementation).
+filebus() {
+	"${BASH}" "${BASH_SOURCE[0]%/*}/filebus.bash" "${@}" || return
+}
+
+# @FUNCTION: filebus-test_setup
+# @DESCRIPTION:
+# Setup sel test program.
+filebus-test_setup() {
+	local mock_state
+	FILEBUS_TEST_TEMPDIR=$(mktemp -d) || return
+	FILEBUS_TEST_DATA_DIR=$FILEBUS_TEST_TEMPDIR/run/brd-tms-conf/filebus || return
+	FILEBUS_TEST_SUPERVISOR_RESULT=12
+	FILEBUS_TEST_DATA_FILE=$FILEBUS_TEST_DATA_DIR/data
+	FILEBUS_TEST_MOCK_STATE_DIR=$FILEBUS_TEST_TEMPDIR/mock-state
+	mkdir -p "$FILEBUS_TEST_DATA_DIR" "$FILEBUS_TEST_TEMPDIR/bin" "$FILEBUS_TEST_MOCK_STATE_DIR".state{1..3} || return
+
+	FILEBUS_TEST_MOCK_STATES=(
+		state1
+		state2
+		state3
+	)
+	FILEBUS_TEST_MOCK_STATES_SEQUENCE=(
+		state1
+		state2
+		state3
+		state2
+		state3
+		state1
+	)
+
+	for mock_state in "${FILEBUS_TEST_MOCK_STATES[@]}"; do
+		printf -- '%s\n' "$mock_state" > "$FILEBUS_TEST_MOCK_STATE_DIR.$mock_state/state" || return
+	done
+	_filebus-test_mutate-mock-state state1 || return
+
+	cat <<-EOF >"$FILEBUS_TEST_TEMPDIR/bin/mock-data-source"
+	#!$BASH
+	exec "$BASH" "${BASH_SOURCE[0]%/*}/filebus.bash" --filename "$FILEBUS_TEST_MOCK_STATE_DIR/state" consumer || exit $?
+	EOF
+	chmod a+x "$FILEBUS_TEST_TEMPDIR/bin/mock-data-source" || return
+
+	FILEBUS_TEST_STATE_MONITOR_COMMAND=("$FILEBUS_TEST_TEMPDIR/bin/mock-data-source")
+
+	export PATH=$FILEBUS_TEST_TEMPDIR/bin:$PATH
+
+	filebus-test_lib-init
+
+	FILEBUS_TEST_WAIT_EXPECTED_COMMAND=(filebus --filename "$FILEBUS_TEST_DATA_FILE" consumer)
+
+	true > "$FILEBUS_TEST_DATA_FILE" || return
+}
+
+# @FUNCTION: filebus-test-cleanup
+# @DESCRIPTION:
+# Self test program.
+filebus-test-cleanup() {
+	rm -rf "$FILEBUS_TEST_TEMPDIR"
+}
+
+# @FUNCTION: filebus-test_process
+# @DESCRIPTION:
+# Self test process.
+filebus-test_process() {
+	local failures=0 mock_state retval=$FILEBUS_TEST_SUPERVISOR_RESULT
+
+	filebus-signal_propagate SIGTERM SIGINT
+	trap filebus-supervisor_cleanup-jobs EXIT || return
+
+	for mock_state in "${FILEBUS_TEST_MOCK_STATES_SEQUENCE[@]}"; do
+		_filebus-test_mutate-mock-state "$mock_state" || return
+		_filebus-test_wait-for-expected "$mock_state"  || return
+	done
+
+	if (( ( retval != 0 && retval != FILEBUS_TEST_SUPERVISOR_RESULT ) || failures != 0 )); then
+		filebus-log_line error "retval: $retval failures: $failures"
+		return 1
+	fi
+
+	return "$retval"
+}
+
+# @FUNCTION: _filebus-test_mutate-mock-state
+# @DESCRIPTION:
+# Self test process.
+_filebus-test_mutate-mock-state() {
+	filebus-log_line info "mutate-mock-state: $1"
+	ln -snf "mock-state.$1" "$FILEBUS_TEST_MOCK_STATE_DIR" || return
+}
+
+# @FUNCTION: _filebus-test_wait-for-expected
+# @DESCRIPTION:
+# Wait for an expected state to occur.
+_filebus-test_wait-for-expected() {
+	local line expected=$1
+	filebus-log_line info "wait-for-expected: expected $expected"
+	while read -r line; do
+		if [[ $line == "$expected" ]]; then
+			break
+		fi
+		filebus-log_line info "wait-for-expected: got $line"
+		filebus-log_line info "wait-for-expected: expected $expected"
+	done < <("${FILEBUS_TEST_WAIT_EXPECTED_COMMAND[@]}")
+}
+
+# @FUNCTION: _filebus-test_supervisor-callback
+# @DESCRIPTION:
+# Self test process.
+# shellcheck disable=SC2031
+_filebus-test_supervisor-callback() {
+	local status=$1
+	filebus-log_line "info" "supervisor callback: ${supervisor_jobs[$pid]} exited with status ${status/${FILEBUS_TEST_SUPERVISOR_RESULT}/FILEBUS_TEST_SUPERVISOR_RESULT}"
+	if [[ ${supervisor_jobs[$pid]} == test_process ]]; then
+		case $status in
+			# Accept 143 for SIGTERM.
+			"$FILEBUS_TEST_SUPERVISOR_RESULT"|143|0)
+				return "$FILEBUS_TEST_SUPERVISOR_RESULT"
+				;;
+		esac
+		return 1
+	elif [[ $status == 141 ]]; then
+		# SIGPIPE is expected.
+		if [[ ${supervisor_jobs[$pid]} == pipe_input_loop ]]; then
+			return "$FILEBUS_TEST_SUPERVISOR_RESULT"
+		fi
+		return 0
+	fi
+	[[ $status == 0 ]]
+}
+
+
+# @FUNCTION: _filebus-test_set-action
+# @DESCRIPTION:
+# Set program action.
+_filebus-test_set-action() {
+	[[ -z ${filebus_test_args[action]:-} ]] || die "Multiple actions specified:" "${filebus_test_args[action]:-}" "${1}"
+	filebus_test_args[action]="${1}"
+}
+
+# @FUNCTION: _filebus-test_set-output-format
+# @DESCRIPTION:
+# Set program output format.
+_filebus-test_set-output-format() {
+	[[ -z ${filebus_test_args[output_format]:-} ]] || die "Multiple output formats specified:" "${filebus_test_args[output_format]:-}" "${1}"
+	filebus_test_args[output_format]="${1}"
+}
+
+# @FUNCTION: _filebus-test_print-usage
+# @DESCRIPTION:
+# Print program usage instructions.
+_filebus-test_print-usage() {
+	printf -- 'usage: filebus %s\n' "$FILEBUS_TEST_CMD_ACTIONS"
+	printf -- '\n'
+	printf -- 'filebus test\n'
+	printf -- '\n'
+	printf -- 'Options:\n'
+	printf -- '  -f, --follow             Follow query topic\n'
+}
+
+# @FUNCTION: _filebus-test_print-usage-query
+# @DESCRIPTION:
+# Print query usage instructions.
+_filebus-test_print-usage-query() {
+	printf -- 'usage: query\n'
+	printf -- '\n'
+	printf -- 'query data\n'
+	printf -- '\n'
+	printf -- 'Options:\n'
+	printf -- '  -f, --follow             follow data stream\n'
+}
+
+# @FUNCTION: filebus-test_state-monitor-loop
+# @DESCRIPTION:
+# State monitor loop.
+filebus-test_state-monitor-loop() {
+	local digest line
+	mkdir -p "${FILEBUS_TEST_DATA_FILE%/*}" || return
+	"${FILEBUS_TEST_STATE_MONITOR_COMMAND[@]}" metrics --follow | while read -r line; do
+		[[ -n $line ]] || continue
+		mkdir -p "${FILEBUS_TEST_DATA_FILE%/*}" || return
+		filebus --filename "$FILEBUS_TEST_DATA_FILE" producer <<< "$line" || return
+	done
+}
+
+# @FUNCTION: _filebus-test_command-perform-query
+# @DESCRIPTION:
+# Perform query command.
+_filebus-test_command-perform-query() {
+	if [[ -n ${follow:-} ]]; then
+		mkdir -p "${FILEBUS_TEST_DATA_FILE%/*}" || return
+		[[ -e "$FILEBUS_TEST_DATA_FILE" ]] || true >> "$FILEBUS_TEST_DATA_FILE" || return
+		filebus --filename "$FILEBUS_TEST_DATA_FILE" consumer || return
+		return
+	fi
+	cat "$FILEBUS_TEST_DATA_FILE" || return
+	return
+}
+
+# @FUNCTION: _filebus-test_command-parser-query
+# @DESCRIPTION:
+# Parser for query command arguments.
+_filebus-test_command-parser-query() {
+	local consumed_args=0
+	while (( $# > 0 )); do
+		if [[ $1 =~ ^(-h|--help|help) ]]; then
+			_filebus-test_print-usage-query || return 1
+			return 0
+		else
+			# break for first unconsumed argument
+			break
+		fi
+	done
+
+	# status
+	# 0       immediate successful exit
+	# 1       immediate failure
+	# 2+      status = consumed_args + 2
+	return $(( consumed_args + 2 ))
+}
+
+# @FUNCTION: filebus-test_main
+# @DESCRIPTION:
+# Main program.
+filebus-test_main() {
+	local arg data_dir follow line output_format pipe pipestatus sub_parser sub_parser_status query_topic
+	local -A filebus_test_args
+	shopt -s lastpipe
+
+	params=$(getopt -o "dfho:" -l "data-dir:,debug,help,follow,output:" --name "${0##*/}" -- "${@}") || exit $?
+	eval set -- "${params}"
+
+	for arg in "${@}"; do
+		case $arg in
+			query|test)
+				if declare -f "_filebus-test_command-parser-$arg" >/dev/null; then
+					sub_parser=_filebus-test_command-parser-$arg
+				fi
+				break
+				;;
+		esac
+	done
+
+	while [[ ${#} -gt 0 ]]; do
+
+		if [[ -n ${sub_parser:-} ]]; then
+			"$sub_parser" "$@"
+			sub_parser_status=$?
+
+			# account for any consumed arguments
+			while (( sub_parser_status > 2 )); do
+				(( sub_parser_status-- ))
+				shift
+			done
+
+			case $sub_parser_status in
+				0)
+					# immediate success
+					return 0
+					;;
+				1)
+					# immediate failure
+					return 1
+					;;
+				2)
+					# status = consumed_args + 2
+					;;
+			esac
+		fi
+
+		(( $# > 0 )) || break
+
+		case $1 in
+			--data-dir)
+				data_dir=${2}
+				shift
+				;;
+			-d|--debug)
+				filebus-log_set-level debug
+				;;
+			-f|--follow)
+				follow=1
+				;;
+			-h|--help|help)
+				_filebus-test_print-usage
+				exit 0
+				;;
+			-o|--output)
+				_filebus-test_set-output-format "${2}"
+				shift
+				;;
+			--)
+				;;
+			query|test)
+				_filebus-test_set-action "${1}"
+				;;
+			*)
+				die "Unexpected argument: $1"
+				;;
+		esac
+		shift
+	done
+
+	if [[ -z ${filebus_test_args[action]:-} ]]; then
+		_filebus-test_print-usage
+		die "Missing required action: $FILEBUS_TEST_CMD_ACTIONS"
+	fi
+
+	if [[ -n ${data_dir:-} ]]; then
+		FILEBUS_TEST_DATA_DIR=$data_dir
+		filebus-test_lib-init
+	fi
+
+
+	cd /
+	filebus-signal_propagate SIGTERM SIGINT
+
+	filebus-log_line "debug" "action: ${filebus_test_args[action]:-}"
+
+	case ${filebus_test_args[action]:-} in
+		query)
+			_filebus-test_command-perform-query || return
+			return
+			;;
+		test)
+			;;
+		*)
+			die "Unknown action: ${filebus_test_args[action]:-}"
+	esac
+
+	[[ ${filebus_test_args[action]} == test ]] && filebus-test_setup
+
+	# shellcheck disable=SC2046
+	disown $(jobs -p) 2>/dev/null
+	trap filebus-supervisor_cleanup-jobs EXIT || return
+
+	local -A supervisor_jobs
+
+	filebus-test_state-monitor-loop &
+	supervisor_jobs[$!]=state-monitor-loop || return
+
+	filebus-test_process &
+	supervisor_jobs[$!]=test_process || return
+	supervisor_callback=_filebus-test_supervisor-callback
+
+	filebus-supervisor_wait "$supervisor_callback"
+	pipestatus=("${PIPESTATUS[@]}")
+
+	case ${filebus_test_args[action]:-} in
+		test)
+			[[ ${pipestatus[*]} == "$FILEBUS_TEST_SUPERVISOR_RESULT" ]]
+			return
+			;;
+		*)
+			[[ ${pipestatus[*]} == "0" ]]
+			return
+			;;
+	esac
+}
+
+if [[ $0 == "${BASH_SOURCE[0]}" ]]; then
+	set -u -o pipefail
+
+	if [[ ${BASH_SOURCE[0]} != /* ]]; then
+		canonical_source=$(realpath --canonicalize-missing "${BASH_SOURCE[0]}")
+		cd /
+		exec "$BASH" "$canonical_source" "${@}" || exit 1
+	fi
+
+	filebus-test_lib-init
+	filebus-test_main "$@"
+fi

commit e9ad031a510a463f3242e83b53d74f850cd6ec0e

lib/bash/filebus.bash: add forgotten flock call
Fixes: a708fdec0f8f ("Add bash implementation of filebus, accessible via --impl=bash")

lib/bash/filebus.bash
@@ -112,7 +112,10 @@ _filebus_command-perform-producer() {
 			fi
 
 			if (( eof == 1 )) || (( read_ready == 0 && buffer_len >= 0 )) || (( buffer_len >= filebus_args[block_size] )); then
+				# shellcheck disable=SC2094
 				({
+					flock --exclusive 200 || exit 1
+					[[ "${filebus_args[filename]}.lock" -ef /dev/fd/200 ]] || exit 1
 					(
 						for line in "${buffer[@]}"; do
 							printf -- '%s\n' "$line" || exit 1
@@ -194,6 +197,7 @@ _filebus_command-perform-consumer() {
 			continue
 		fi
 		if (( filebus_args[back_pressure] == 1 )); then
+			# shellcheck disable=SC2094
 			(
 				{
 					flock --exclusive 200 || exit 1

lib/python/filebus/__init__.py
@@ -25,7 +25,7 @@ except ImportError:
     watchdog = None
     FileSystemEventHandler = object
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __project__ = "filebus"
 __description__ = (
     "A user space multicast named pipe implementation backed by a regular file"