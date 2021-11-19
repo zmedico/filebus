30866563251beec9253cd01fa4e26c30cee86392
Zac Medico <zmedico@gmail.com>
2021-11-18 19:35:10 -0800

    bash: indent producer back pressure blocking loop

--- lib/bash/filebus.bash
+++ lib/bash/filebus.bash
@@ -113,48 +113,48 @@ _filebus_command-perform-producer() {
 
 			if (( eof == 1 )) || (( read_ready == 0 && buffer_len > 0 )) || (( buffer_len >= filebus_args[block_size] )); then
 				while true; do
-				if (( filebus_args[back_pressure] == 1 )); then
-					while [[ -e ${filebus_args[filename]} ]]; do
-						sleep "${filebus_args[sleep_interval]}" || return
-					done
-				fi
-				# shellcheck disable=SC2094
-				({
-					flock --exclusive 200 || exit 1
-					[[ "${filebus_args[filename]}.lock" -ef /dev/fd/200 ]] || exit 1
-					if (( filebus_args[back_pressure] == 1 )) && [[ -e ${filebus_args[filename]} ]]; then
-						exit 3 # back pressure blocking
-					fi
-					(
-						for line in "${buffer[@]}"; do
-							printf -- '%s\n' "$line" || exit 1
+					if (( filebus_args[back_pressure] == 1 )); then
+						while [[ -e ${filebus_args[filename]} ]]; do
+							sleep "${filebus_args[sleep_interval]}" || return
 						done
-					) > "${filebus_args[filename]}.__new__" || exit 1
-					mv "${filebus_args[filename]}"{.__new__,} || exit 1
-					if (( filebus_args[back_pressure] == 0 )); then
-						exit 0
-					elif [[ ! -s ${filebus_args[filename]} ]]; then
-						exit 2 # back pressure protocol EOF marker
 					fi
-					exit 0
-				} 200>"${filebus_args[filename]}.lock")
-				case $? in
-					0)
-						buffer=()
-						buffer_len=0
-						break
-						;;
-					1)
-						return 1
-						;;
-					2)	# back pressure protocol EOF marker
-						return 0
-						;;
-					3)	# back pressure blocking
-						sleep "${filebus_args[sleep_interval]}" || return
-						continue
-						;;
-				esac
+					# shellcheck disable=SC2094
+					({
+						flock --exclusive 200 || exit 1
+						[[ "${filebus_args[filename]}.lock" -ef /dev/fd/200 ]] || exit 1
+						if (( filebus_args[back_pressure] == 1 )) && [[ -e ${filebus_args[filename]} ]]; then
+							exit 3 # back pressure blocking
+						fi
+						(
+							for line in "${buffer[@]}"; do
+								printf -- '%s\n' "$line" || exit 1
+							done
+						) > "${filebus_args[filename]}.__new__" || exit 1
+						mv "${filebus_args[filename]}"{.__new__,} || exit 1
+						if (( filebus_args[back_pressure] == 0 )); then
+							exit 0
+						elif [[ ! -s ${filebus_args[filename]} ]]; then
+							exit 2 # back pressure protocol EOF marker
+						fi
+						exit 0
+					} 200>"${filebus_args[filename]}.lock")
+					case $? in
+						0)
+							buffer=()
+							buffer_len=0
+							break
+							;;
+						1)
+							return 1
+							;;
+						2)	# back pressure protocol EOF marker
+							return 0
+							;;
+						3)	# back pressure blocking
+							sleep "${filebus_args[sleep_interval]}" || return
+							continue
+							;;
+					esac
 				done
 			fi
 			if (( buffer_len == 0 )); then
